e74ee1a73978eee30a7b339673409f52342b9e50
LAPTOP-92NBAG1S\lenovo <nomai6789@gmail.com>
2025-07-16 14:46:44 +0700

    first commit

---
+++ src/main/java/vn/edu/iuh/fit/antoreechatbotbackend/AntoreeChatbotBackendApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.antoreechatbotbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntoreeChatbotBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AntoreeChatbotBackendApplication.class, args);
+    }
+
+}
